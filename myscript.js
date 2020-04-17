3efd79e0484d3a737d88a5cf90a5861bea83e405
Veronica Celseti <veronicacelseti12@gmail.com>
2020-04-17 14:20:18 +0200

    dichiarazione variabili

--- myscript.js
+++ myscript.js
@@ -1,5 +1,15 @@
 //chiedere il nome
+var nome_utente = prompt('Qual è il tuo nome?');
+console.log(nome_utente);
 
 //chiedere il cognome
+var cognome_utente = prompt('Qual è il tuo cognome?');
+console.log(cognome_utente);
+document.writeln( 'Ciao ' + nome_utente + ' ' + cognome_utente);
+
 //chiedere il colore preferito
+var colore_preferito_utente = prompt('Qual è il tuo colore preferito?');
+console.log(colore_preferito_utente);
+
 //visualizzare la password nomecognomecolorepreferito20
+document.writeln('La tua password è ' + nome_utente + cognome_utente + colore_preferito_utente + '20');
